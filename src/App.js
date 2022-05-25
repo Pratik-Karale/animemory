556f89d63737ac337252e2ab3852fc956ea92c84
pratik-k <prakar07@protonmail.com>
2022-05-25 14:27:02 +0530

    import add and delete externally imported data names

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-
+import { cardsData } from "./cardData";
+// const getImageURLs=(n)=>fetch("https://api.jikan.moe/v4/anime/21/characters").then(resp=>resp.json()).then(data=>data.data.slice(0,n).map((charObj)=>charObj.character.images.jpg.image_url))
 function App() {
   return (
     <div className="App">

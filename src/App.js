5fac48ac9dac646f736d635ee9d9a1c1a02e97b1
pratik-k <prakar07@protonmail.com>
2022-05-25 14:32:38 +0530

    add score state and its functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import { cardsData } from "./cardData";
+import {useState} from "react"
+import { Score } from "./Score";
 // const getImageURLs=(n)=>fetch("https://api.jikan.moe/v4/anime/21/characters").then(resp=>resp.json()).then(data=>data.data.slice(0,n).map((charObj)=>charObj.character.images.jpg.image_url))
 function App() {
+  const [score,setScore]=useState(0)
+  const incrementScore=()=>setScore(score+1)
+  const playerLost=()=>{
+    setScore(0)
+  }
   return (
     <div className="App">
-      
+      <Score score={score}/>
     </div>
   );
 }
